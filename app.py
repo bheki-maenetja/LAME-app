747e6696cdc025678f23306d641269fe1ec77456
bheki-maenetja <bhekimaenetja@gmail.com>
2023-02-26 11:01:26 +0000

    started to add major components; upload widget, tabs

--- app.py
+++ app.py
@@ -10,10 +10,28 @@ app.title = "Lexical Analyser Manipulator and Extractor (LAME)"
 
 server = app.server
 
+upload_text = "Drag and drop, or click to upload files to storage"
+
 # UI Layout
 ## Main App Layout (headings, file saver and file selector)
-app.layout = html.Div(children=[
-    html.H1("Welcome to LAME!!!")
+app.layout = html.Div(id="main-container", children=[
+    html.H1("Welcome to LAME!!!"),
+    dcc.Upload(
+        id="upload-data",
+        multiple=True,
+        children=dcc.Loading(
+            children=[
+                html.Div(upload_text)
+            ]
+        )
+    ),
+    dcc.Tabs(id="main-tabs", value="1", children=[
+        dcc.Tab(label="My Documents", value="1"),
+        dcc.Tab(label="Information Extraction", value="2"),
+        dcc.Tab(label="Summarisation", value="3"),
+        dcc.Tab(label="Clustering", value="4"),
+        dcc.Tab(label="WikiBot", value="5"),
+    ])
 ])
 
 # Running server
