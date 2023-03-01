8e2567b5c2e4ce504241733f225b1ddbcda765ab
bheki-maenetja <bhekimaenetja@gmail.com>
2023-03-01 11:31:18 +0000

    stumbled on to solution for pdf parsing; need to expand

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -5,6 +5,7 @@ import docx2txt
 # Standard Library Imports
 import os
 import base64
+import io
 
 # Local Imports
 
@@ -13,11 +14,31 @@ def save_files(f_names, f_contents):
     for f_name, f_content in zip(f_names, f_contents):
         extension = os.path.splitext(f_name)[1]
         if extension == ".pdf":
-            text = f_content.encode("utf8").split(b";base64,")[1]
+            f_text = f_content.encode("utf8").split(b";base64,")[1]
             # print(f_name, extension, base64.decodetext, end=f"\n\n{50*'='}\n\n")
-            decoded_text = base64.decodebytes(text)
+            # text = f_content.split('data:application/pdf;base64,')[1]
+            # decoded_text = base64.b64decode(text)
+            # print(decoded_text)
             # decoded_content = base64.b64decode(f_content)
-            print(decoded_text)
             # with open(os.path.join(), "rb") as pdf_pointer:
             #     text = PdfReader(pdf_pointer)
             #     print(name, text, end=f"\n\n{50*'='}")
+            # with io.BytesIO(f_text) as f:
+            #     pdf_reader = PdfReader(f)
+            #     num_pages = pdf_reader.getNumPages()
+            #     text = ""
+            #     for i in range(num_pages):
+            #         page = pdf_reader.getPage(i)
+            #         text += page.extractText()
+            # print(text)
+            data = f_content.encode("utf8").split(b";base64,")[1]
+            with open(os.path.join('cache', f_name), "wb") as fp:
+                fp.write(base64.decodebytes(data))
+            
+            pdf_reader = PdfReader(os.path.join('cache', f_name))
+            num_pages = len(pdf_reader.pages)
+            text = ""
+            for i in range(num_pages):
+                page = pdf_reader.pages[i]
+                text += page.extract_text()
+            print(text)
