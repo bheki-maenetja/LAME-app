e92e619568b048eb10c122201e019abcf11eb642
bheki-maenetja <bhekimaenetja@gmail.com>
2023-03-01 15:08:49 +0000

    developed way to extract text from .pdf, .docx, .md and .txt files; moving on to finding storage solution

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -13,32 +13,29 @@ import io
 def save_files(f_names, f_contents):
     for f_name, f_content in zip(f_names, f_contents):
         extension = os.path.splitext(f_name)[1]
+
+        data = f_content.encode("utf8").split(b";base64,")[1]
+        with open(os.path.join('temp', f_name), "wb") as fp:
+            fp.write(base64.decodebytes(data))
+        
         if extension == ".pdf":
-            f_text = f_content.encode("utf8").split(b";base64,")[1]
-            # print(f_name, extension, base64.decodetext, end=f"\n\n{50*'='}\n\n")
-            # text = f_content.split('data:application/pdf;base64,')[1]
-            # decoded_text = base64.b64decode(text)
-            # print(decoded_text)
-            # decoded_content = base64.b64decode(f_content)
-            # with open(os.path.join(), "rb") as pdf_pointer:
-            #     text = PdfReader(pdf_pointer)
-            #     print(name, text, end=f"\n\n{50*'='}")
-            # with io.BytesIO(f_text) as f:
-            #     pdf_reader = PdfReader(f)
-            #     num_pages = pdf_reader.getNumPages()
-            #     text = ""
-            #     for i in range(num_pages):
-            #         page = pdf_reader.getPage(i)
-            #         text += page.extractText()
-            # print(text)
-            data = f_content.encode("utf8").split(b";base64,")[1]
-            with open(os.path.join('cache', f_name), "wb") as fp:
-                fp.write(base64.decodebytes(data))
-            
-            pdf_reader = PdfReader(os.path.join('cache', f_name))
+            pdf_reader = PdfReader(os.path.join('temp', f_name))
             num_pages = len(pdf_reader.pages)
             text = ""
             for i in range(num_pages):
                 page = pdf_reader.pages[i]
                 text += page.extract_text()
             print(text)
+        elif extension == ".docx":
+            text = docx2txt.process(os.path.join('temp', f_name))
+            print(text)
+        elif extension in (".txt", ".md"):
+            with open(os.path.join('temp', f_name), "r") as file:
+                text = file.read()
+            print(text)
+        else:
+            print(f"Unsupported file extension '{extension}'")
+            return None
+    
+    for file in os.listdir('temp'):
+        os.remove(os.path.join('temp', file))
