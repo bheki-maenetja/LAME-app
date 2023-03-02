b0b3b9e2b13219f3b0351ff6cf50224d8ff3375b
bheki-maenetja <bhekimaenetja@gmail.com>
2023-03-02 12:32:36 +0000

    still getting a hang of cloudinary

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -2,17 +2,31 @@
 from PyPDF2 import PdfReader
 import docx2txt
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Standard Library Imports
 import os
 import base64
-import io
 
 # Local Imports
+
+# Cloudinary configuration
+cloudinary.config(
+    cloud_name = os.getenv("CLOUD_NAME"),
+    api_key = os.getenv("CLOUD_KEY"),
+    api_secret = os.getenv("CLOUD_SECRET"),
+    secure = True
+)
 
 # Reading and Writing Data
 def save_files(f_names, f_contents):
     for f_name, f_content in zip(f_names, f_contents):
-        extension = os.path.splitext(f_name)[1]
+        doc_title, extension = os.path.splitext(f_name)
 
         data = f_content.encode("utf8").split(b";base64,")[1]
         with open(os.path.join('temp', f_name), "wb") as fp:
@@ -36,6 +50,17 @@ def save_files(f_names, f_contents):
         else:
             print(f"Unsupported file extension '{extension}'")
             return None
+        
+        # meta_data = {
+        #     "title": doc_title,
+        #     "raw_text": text
+        # }
+        print(doc_title)
+        cloudinary.uploader.upload(
+            os.path.join("temp", f_name),
+            display_name=doc_title, 
+            folder="LAME_upload",
+        )
     
     for file in os.listdir('temp'):
         os.remove(os.path.join('temp', file))
