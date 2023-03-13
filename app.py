7d343a4b2acab5f5518d5542d36e15e807c280dc
bheki-maenetja <bhekimaenetja@gmail.com>
2023-03-13 09:50:13 +0000

    added and styled all major UI components on summarisation page

--- app.py
+++ app.py
@@ -253,8 +253,8 @@ def get_info_extraction_section():
         )
 
     docs_copy = docs.copy(deep=True)
-
     doc_list = docs_copy["title"]
+
     return html.Div(
         id="info-extract-section",
         children=[
@@ -324,12 +324,92 @@ def get_summary_section():
             className="no-docs-heading"
         )
 
+    docs_copy = docs.copy(deep=True)
+    doc_list = docs_copy["title"]
+
     return html.Div(
         id="summary-section",
         children=[
-            html.H3("Summarisation")
-        ]
-    )
+            html.Div(
+                id="summary-params",
+                children=[
+                    html.Div(
+                        children=[
+                            html.H3("Select Documents"),
+                            dcc.Dropdown(
+                                id="summary-doc-select",
+                                options=doc_list,
+                                multi=True,
+                                placeholder="Select documents..."
+                            )
+                        ]
+                    ),
+                    html.Div(
+                        children=[
+                            html.H3("Method"),
+                            dcc.Dropdown(
+                                id="summary-method-select",
+                                options=[
+                                    {"label": "Simple Extractive", "value": "se"}, 
+                                    {"label": "LexRank", "value": "lexR"},
+                                    {"label": "TextRank", "value": "textR"},
+                                    {"label": "Latent Semenatic Analysis", "value": "lsa"},
+                                    {"label": "Luhn's Algorithm", "value": "luhn"},
+                                    {"label": "BART", "value": "bart"},
+                                    {"label": "T5", "value": "t5"},
+                                    {"label": "Longformer", "value": "longf"},
+                                    {"label": "OpenAI", "value": "openai"},
+                                ],
+                                value="se",
+                                multi=False,
+                                clearable=False,
+                            )
+                        ]
+                    ),
+                    html.Div(
+                        children=[
+                            html.H3("Summary Size (% of Original)"),
+                            dcc.Slider(
+                                id="summary-size-slider",
+                                min=5,
+                                max=100,
+                                step=5,
+                                value=50,
+                                included=False,
+                                marks=None,
+                                tooltip={
+                                    "placement": "bottom", 
+                                    "always_visible": True
+                                }
+                            )
+                        ]
+                    ),
+                    html.Button(
+                        id="summary-btn",
+                        children="Summarise Documents",
+                        className="nlp-btn-disabled",
+                        disabled=True,
+                    )
+                ]
+            ),
+            html.Div(
+                id="summary-output",
+                children=[
+                    dcc.Loading(
+                        color="#003049",
+                        children=[
+                            dbc.Textarea(
+                                id="summary-output-content",
+                                value="",
+                                draggable=False,
+                                readOnly=True,
+                                placeholder="The summary of your document(s) will appear here."
+                            )
+                        ])
+                    ]
+                )
+            ]
+        )
 
 ### Clustering Section
 def get_clustering_section():
