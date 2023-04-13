ca159d04bc1e2111fa10a2a9be293f510083a667
bheki-maenetja <bhekimaenetja@gmail.com>
2023-04-13 13:21:07 +0200

    Create doc_clustering_section.py

---
+++ user_interface/doc_clustering_section.py
@@ -0,0 +1,63 @@
+# Third-Party Imports
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+# Standard Imports
+# Local Imports
+import nlp.doc_clustering as dc
+
+def get_clustering_section(docs):
+    if docs is None or len(docs) < 2:
+        text = "Please upload 2 or more documents to get started with document clustering."
+        return html.H2(
+            children=text,
+            className="no-docs-heading"
+        )
+
+    docs_copy = docs.copy(deep=True)
+    doc_list = docs_copy["title"]
+
+    return html.Div(
+        id="clustering-section",
+        children=[
+            html.Div(
+                id="clustering-params",
+                children=[
+                    dcc.Dropdown(
+                        id="clustering-doc-select",
+                        options=doc_list,
+                        multi=True,
+                        placeholder="Select documents..."
+                    ),
+                    dcc.Dropdown(
+                        id="clustering-num-select", 
+                        options=[
+                            {"label": str(i), "value": i}
+                            for i in range(2, len(doc_list) + 1)
+                        ],
+                        multi=False,
+                        placeholder="Number of clusters",
+                        clearable=False,
+                        value=2,
+                    ),
+                    html.Button(
+                        id="clustering-btn",
+                        children="Create Visualisation",
+                        className="nlp-btn-disabled",
+                        disabled=True,
+                    )
+                ]
+            ),
+            html.Div(
+                id="clustering-fig-container",
+                children=[
+                    dcc.Loading([
+                        dcc.Graph(
+                            id="clustering-plot",
+                            figure=dc.plot_data()
+                        )
+                    ])
+                ]
+            )
+        ]
+    )
