63d8f2148e662ccfed7ac11911ab25649a9e8e83
bheki-maenetja <bhekimaenetja@gmail.com>
2023-07-29 19:25:54 +0200

    redeployment; 2nd attempt

--- web-tasks.sh
+++ web-tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod a+x web-tasks.sh
-python -m nltk.downloader all
-python -m spacy download en_core_web_lg
-python -m spacy download en_core_web_sm
+# python -m nltk.downloader all
+# python -m spacy download en_core_web_lg
+# python -m spacy download en_core_web_sm
 gunicorn app:server --timeout 60 --keep-alive 5
