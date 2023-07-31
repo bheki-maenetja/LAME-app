e1e613bff7f03ffbcfc8e29af6e52b9d4917d743
bheki-maenetja <bhekimaenetja@gmail.com>
2023-07-31 09:39:10 +0200

    Update web-tasks.sh

--- web-tasks.sh
+++ web-tasks.sh
@@ -3,4 +3,5 @@ chmod a+x web-tasks.sh
 python -m nltk.downloader all
 python -m spacy download en_core_web_lg
 python -m spacy download en_core_web_sm
-gunicorn app:server --timeout 60 --keep-alive 5
+# gunicorn app:server --timeout 60 --keep-alive 5
+python app.py
