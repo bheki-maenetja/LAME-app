8602391ab7bb90cdbd3fa45dd9c86faf0a1b92e9
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-07 11:27:10 +0200

    fixed minor error with building corpus

--- user_interface/wikibot_section.py
+++ user_interface/wikibot_section.py
@@ -2,76 +2,18 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-def get_wiki_bot_section(tagline):
-    return html.Div(
-        id="wikibot-section",
-        children=[
-            html.I(
-                id="wikibot-icon", 
-                className="bi bi-wikipedia", 
-                children=[html.Span("ikiBot")],
-            ),
-            html.H3(id="wikibot-tagline", children=tagline),
-            html.Div(
-                id="wikibot-params",
-                children=[
-                    dbc.Input(
-                        id="wikibot-query",
-                        placeholder="Enter your question here",
-                        value="",
-                        persistence=False,
-                    ),
-                    dbc.Select(
-                        id="wikibot-method-select",
-                        placeholder="Select a Search Method",
-                        options=[
-                            {"label": "TF-IDF", "value": "tf-idf"}, 
-                            {"label": "Cosine Similarity", "value": "cosine_sim"},
-                            {"label": "BERT", "value": "bert"},
-                            {"label": "OpenAI", "value": "openai"}
-                        ],
-                        value=None,
-                        persistence=False,
-                    ),
-                    html.Button(
-                        id="wikibot-btn",
-                        children="Search",
-                        className="nlp-btn-disabled",
-                        disabled=True,
-                    )
-                ]
-            ),
-            html.Div(
-                id="wikibot-output",
-                children=[
-                    dcc.Loading(
-                        color="#003049",
-                        children=[
-                            dbc.Textarea(
-                                id="wikibot-output-content",
-                                value="",
-                                draggable=False,
-                                readOnly=True,
-                                placeholder="The results of your query will appear here"
-                            )
-                        ]
-                    )
-                ]
-            )
-        ]
-    )
-
 def create_wiki_bot_message(message, is_user=True, is_temp=False):
     if is_temp:
         return html.Div(
             id="temp-message",
             className="wikibot-message bot",
-            hidden=True,
             children=[
-                html.Div(
-                    className=f"wikibot-message-content bot",
-                    children="Temporary...placeholder for something else!"
+                dbc.Spinner(
+                    color="warning", 
+                    type="grow", 
+                    spinner_style={"marginRight": "0.5em"}
                 )
+                for _ in range(3)
             ]
         )
     return html.Div(
@@ -84,7 +26,7 @@ def create_wiki_bot_message(message, is_user=True, is_temp=False):
         ]
     )
 
-def get_wiki_bot_section_2(tagline):
+def get_wiki_bot_section(tagline):
     return html.Div(
         id="wikibot-section",
         children=[
